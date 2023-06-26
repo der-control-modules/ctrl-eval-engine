1df7e195ec584a96f40e114189c3340996e18b0c
Kini, Roshan L <roshan.kini@pnnl.gov>
2023-06-26 10:55:43 -0700

    updated amac to run in a loop

--- app/src/realtime-controller/amac.py
+++ app/src/realtime-controller/amac.py
@@ -56,6 +56,19 @@ class AMACOperation:
         self.asc_power = 0
         self.battery_output_power = 0
         self.acceleration_parameter = 0
+        self.max_interval_length = 900
+        self.load_total_data = TSBuffer(maxlen=self.max_interval_length)
+    
+        # BESS default values   
+        self.bess_rated_kw = 125.0
+        self.bess_rated_kwh = 200.0
+        self.bess_eta = 0.925
+        self.bess_soc_max = 90
+        self.bess_soc_min = 10
+        self.asc_power = 0
+        self.battery_power = 0
+        self.acceleration_parameter = 0
+        self.soc_pct = 50
         
     def get_usecase_config(self, usecase_config):
         # TODO: move maximumPvPower and dependent variables out of __init__
@@ -77,19 +90,6 @@ class AMACOperation:
         ) / 100
         self.ref_variability = (maximum_pv_power * reference_variability_pct) / 100
 
-        self.bess_rated_kw = 125.0
-        self.bess_rated_kwh = 200.0
-        self.bess_eta = 0.925
-        self.bess_soc_max = 90
-        self.bess_soc_min = 10
-        self.asc_power = 0
-        self.battery_power = 0
-        self.acceleration_parameter = 0
-        self.soc_pct = 50
-
-        self.max_interval_length = 900
-        self.load_total_data = TSBuffer(maxlen=self.max_interval_length)
-
     def get_bess_data(self, rated_kw, rated_kwh, eta, soc_pct, soc_max, soc_min):
         # BESS config
         self.bess_rated_kw = rated_kw
@@ -103,15 +103,16 @@ class AMACOperation:
         self.load_power = load_data
         self.d_time = d_time
         self.load_total_data.append(self.load_power, self.d_time)
+        #print(self.load_power)
 
-    def publish_calculations(self, value_buffer, horizon=2):
+    def publish_calculations(self, value_buffer, horizon=900):
         if len(value_buffer) < horizon:
             return
         value_series = value_buffer.get_series()
         rolling_power = value_series.rolling(min_periods=horizon, window=horizon)
 
-        self.mean = rolling_power.mean()[-1]
-        self.variability = rolling_power.std()[-1]
+        self.mean = rolling_power.mean().values[-1]
+        self.variability = rolling_power.std().values[-1]
 
     def persistence(self, buf, window_size, forecast_delta=None):
         forecast_value, forecast_time = None, None
@@ -119,82 +120,87 @@ class AMACOperation:
             try:
                 # TODO: Migrate to new time_series_buffer which uses since datetime instead of number of data.
                 series = buf.get_series()
-                forecast_value = series.rolling(
+                #print(series)
+                forecast_values = series.rolling(
                     window=window_size, min_periods=1
-                ).mean()[-1]
-                forecast_time = series.index[-1] + forecast_delta
+                ).mean()
+                #print(f"forecast values = {forecast_values}")
+                forecast_value = forecast_values.values[-1]
+               # print(f"forecast value = {forecast_value}")
+                #forecast_time = series.index[-1] + forecast_delta
                 # times, data = buf.get(horizon)
                 # forecast_value, forecast_time = np.mean(data), times[-1] + timedelta(seconds=horizon)
             except Exception as e:
                 print("Exception in smart_persistence: {}".format(str(e)))
-        return forecast_value, forecast_time
+        return forecast_value
 
     def calculate_soc(self, soc_now, power):
         return (power / (self.bess_rated_kwh * self.data_interval) / 36) + soc_now
 
-    def run_model(self):
+    def run_model(self, soc_pct):
         self.publish_calculations(self.load_total_data)
         # A window size derived from std.
-        window_size = (
+        window_size = int((
             self.max_window_size * (self.variability - self.min_variability)
         ) / (
             self.variability
             + ((self.max_variability - self.variability) / self.damping_parameter)
-        )
+        ))
 
         instantaneous_residual, horizon = 0, 0
+        if window_size > 2:
+            #print(f"window_size = {window_size}")
+            if self.variability > self.min_variability:
+                self.acceleration_parameter = min(
+                    (self.variability - self.min_variability)
+                    / (self.ref_variability - self.min_variability),
+                    1,)
+            else:
+                self.acceleration_parameter = 0
 
-        if self.variability > self.min_variability:
-            self.acceleration_parameter = min(
-                (self.variability - self.min_variability)
-                / (self.ref_variability - self.min_variability),
-                1,)
-        else:
-            self.acceleration_parameter = 0
-
-        delta_soc = float(self.soc_pct) - float(self.bess_soc_ref)
-        sign = 1 if delta_soc <= 0 else -1
-        if abs(delta_soc) > 0:
-            self.asc_power = (
-                sign
-                * self.bess_rated_kw
-                * self.acceleration_parameter
-                * min(1, (abs(delta_soc) / (self.bess_soc_max - self.bess_soc_ref)))
-            )
-        else:
-            self.asc_power = 0
-
-        if self.variability < self.min_variability:
-            self.asc_power = 0
-
-        ama_power = 0
-        if window_size > 0:
-            residuals = []
-            for horizon in [
-                timedelta(seconds=self.data_interval),
-                timedelta(seconds=window_size),
-            ]:
-                # TODO: This should have a setting for the meter to use, or should only be reading one if appropriate.
-                residual_forecast, residual_forecast_time = self.persistence(
-                    self.load_total_data,
-                    window_size=horizon,
-                    forecast_delta=timedelta(seconds=self.data_interval),
+            delta_soc = float(soc_pct) - float(self.bess_soc_ref)
+            sign = 1 if delta_soc <= 0 else -1
+            if abs(delta_soc) > 0:
+                self.asc_power = (
+                    sign
+                    * self.bess_rated_kw
+                    * self.acceleration_parameter
+                    * min(1, (abs(delta_soc) / (self.bess_soc_max - self.bess_soc_ref)))
                 )
-                residuals.append(residual_forecast)
-            ama_power = residuals[0] - residuals[1]
-            instantaneous_residual = ama_power + self.asc_power
-            self.battery_power = self.load_power - instantaneous_residual
-        new_soc = self.calculate_soc(self.battery_power, self.soc_pct)
+            else:
+                self.asc_power = 0
 
-        message = [
-            {
-                "load_len": len(self.load_total_data),
-                # "load": self.load_total_data.get_series() if len(self.load_total_data) > 0 else None,
-                "ama_power": ama_power,
-                "battery_power": self.battery_power,
-                "instantaneous_residual": instantaneous_residual,
-                "window": window_size,
-            }
-        ]
-        print(message)
-        return new_soc, instantaneous_residual, self.battery_power, window_size
+            if self.variability < self.min_variability:
+                self.asc_power = 0
+
+            ama_power = 0
+            if window_size > 0:
+                residuals = []
+                for horizon in [self.data_interval, window_size]:
+                    #print(f"horizon = {horizon}")
+                    # TODO: This should have a setting for the meter to use, or should only be reading one if appropriate.
+                    residual_forecast = self.persistence(
+                        self.load_total_data,
+                        window_size=horizon,
+                        forecast_delta=timedelta(seconds=self.data_interval),
+                    )
+                    #print(f"residual_forecast = {residual_forecast}")
+                    residuals.append(residual_forecast)
+                self.ama_power = residuals[0] - residuals[1]
+                instantaneous_residual = ama_power + self.asc_power
+                self.battery_power = self.load_power - instantaneous_residual
+
+            message = [
+                {
+                    #"load_len": len(self.load_total_data),
+                    "load": self.load_total_data.get_series() if len(self.load_total_data) > 0 else None,
+                    "ama_power": ama_power,
+                    "battery_power": self.battery_power,
+                    "instantaneous_residual": instantaneous_residual,
+                    "window": window_size,
+                }
+            ]
+            #print(message)
+            return self.battery_power
+    
+        return 0
