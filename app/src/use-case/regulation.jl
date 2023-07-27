6cebbb382dc64464c7622b51e8658253fd2f145c
Dexin Wang <dexin.wang@pnnl.gov>
2023-07-27 12:29:54 -0700

    Define zero functions for relevant regulation types

--- app/src/use-case/regulation.jl
+++ app/src/use-case/regulation.jl
@@ -4,35 +4,40 @@ struct RegulationPricePoint
     servicePrice::Float64
 end
 
-struct RegulationOperationPoint{TC<:Union{Real,JuMP.VariableRef},TM<:Union{Real,JuMP.VariableRef}}
+Base.:zero(RegulationPricePoint) = RegulationPricePoint(0, 0)
+
+struct RegulationOperationPoint{
+    TC<:Union{Real,JuMP.VariableRef},
+    TM<:Union{Real,JuMP.VariableRef},
+}
     capacity::TC
     mileage::TM
 end
+
+Base.:zero(RegulationOperationPoint) = RegulationOperationPoint(0, 0)
+
 struct Regulation <: UseCase
     price::FixedIntervalTimeSeries{Hour,RegulationPricePoint}
     performanceScore::Float64
 end
 
-function regulation_income(regOperation::TimeSeries{<:RegulationOperationPoint}, ucReg::Regulation)
+function regulation_income(
+    regOperation::TimeSeries{<:RegulationOperationPoint},
+    ucReg::Regulation,
+)
     (ucReg.price ⋅ regOperation) * ucReg.performanceScore
 end
 
 import Base: *
-*(pp::RegulationPricePoint, op::RegulationOperationPoint) = pp.capacityPrice * op.capacity + pp.servicePrice * op.capacity * op.mileage
-*(op::RegulationOperationPoint, pp::RegulationPricePoint) = pp.capacityPrice * op.capacity + pp.servicePrice * op.capacity * op.mileage
+*(pp::RegulationPricePoint, op::RegulationOperationPoint) =
+    pp.capacityPrice * op.capacity + pp.servicePrice * op.capacity * op.mileage
+*(op::RegulationOperationPoint, pp::RegulationPricePoint) =
+    pp.capacityPrice * op.capacity + pp.servicePrice * op.capacity * op.mileage
 
 function calculate_metrics(operation::OperationHistory, ucReg::Regulation)
     return [
-        Dict(
-            :sectionTitle => "Frequency Regulation",
-        ),
-        Dict(
-            :label => "Revenue (ex.)",
-            :value => "\$4363.35"
-        ),
-        Dict(
-            :label => "AGC Signal Following RMSE (ex.)",
-            :value => 1.34e-3
-        ),
+        Dict(:sectionTitle => "Frequency Regulation"),
+        Dict(:label => "Revenue (ex.)", :value => "\$4363.35"),
+        Dict(:label => "AGC Signal Following RMSE (ex.)", :value => 1.34e-3),
     ]
 end
