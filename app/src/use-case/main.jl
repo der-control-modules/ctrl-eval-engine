52e09f5fabb7f84c06c80de1e38e2a3caaa343f4
Dexin Wang <dexin.wang@pnnl.gov>
2023-07-27 12:01:22 -0700

    Add calculations for variability

--- app/src/use-case/main.jl
+++ app/src/use-case/main.jl
@@ -42,7 +42,7 @@ get_use_cases(inputDict::Dict, setting::SimSetting) = [
     if name === "Energy Arbitrage"
         EnergyArbitrage(config["data"], setting.simStart, setting.simEnd)
     elseif name === "Variability Mitigation"
-        VariabilityMitigation(config)
+        VariabilityMitigation(config, setting.simStart, setting.simEnd)
     else
         throw(InvalidInput("Unknown use case: $name"))
     end
--- app/src/use-case/variability-mitigation.jl
+++ app/src/use-case/variability-mitigation.jl
@@ -12,18 +12,51 @@ Construct an `VariabilityMitigation` object from `input` dictionary or array
 VariabilityMitigation(config::Dict) = VariabilityMitigation(
     FixedIntervalTimeSeries(
         DateTime(config["pvGenProfile"][1]["DateTime"]),
-        DateTime(config["pvGenProfile"][2]["DateTime"]) - DateTime(config["pvGenProfile"][1]["DateTime"]),
-        [Float64(row["Power"]) for row in config["pvGenProfile"]]
+        DateTime(config["pvGenProfile"][2]["DateTime"]) -
+        DateTime(config["pvGenProfile"][1]["DateTime"]),
+        [Float64(row["Power"]) for row in config["pvGenProfile"]],
     ),
-    config["ratedPowerKw"]
+    config["ratedPowerKw"],
 )
 
+VariabilityMitigation(config::Dict, tStart::DateTime, tEnd::DateTime) =
+    VariabilityMitigation(
+        extract(
+            FixedIntervalTimeSeries(
+                DateTime(config["pvGenProfile"][1]["DateTime"]),
+                DateTime(config["pvGenProfile"][2]["DateTime"]) -
+                DateTime(config["pvGenProfile"][1]["DateTime"]),
+                [Float64(row["Power"]) for row in config["pvGenProfile"]],
+            ),
+            tStart,
+            tEnd,
+        ),
+        config["ratedPowerKw"],
+    )
 
-calculate_metrics(op::OperationHistory, ucVM::VariabilityMitigation) = [
+moving_std(ts::TimeSeries, windowLength::Dates.TimePeriod, samplingRate::Dates.TimePeriod) =
+    begin
+        tStart = start_time(ts) + windowLength
+        # if tStart > end_time(ts)
+        #     return FixedIntervalTimeSeries(tStart, samplingRate, [])
+        # end
+
+        v = [std(ts, t - windowLength, t) for t = tStart:samplingRate:end_time(ts)]
+        return FixedIntervalTimeSeries(tStart, samplingRate, v)
+    end
+
+calculate_metrics(op::OperationHistory, ucVM::VariabilityMitigation) = begin
+    essPower = power(op)
+    netPowerSmooth = essPower + ucVM.pvGenProfile
+    originalMaxVariability = maximum(moving_std(ucVM.pvGenProfile, Minute(10), ucVM.pvGenProfile.resolution))
+    smoothedMaxVariability = maximum(moving_std(netPowerSmooth, Minute(10), ucVM.pvGenProfile.resolution))
+    mitigatedVariabilityPct = (originalMaxVariability - smoothedMaxVariability) / originalMaxVariability * 100
+
+    [
         Dict(:sectionTitle => "Variability Mitigation"),
-        Dict(:label => "Mitigated Variability", :value => "0%"),
+        Dict(:label => "Mitigated Variability", :value => "$(mitigatedVariabilityPct)%"),
         Dict(:label => "SOC Deviation", :value => "0%"),
     ]
+end
 
-use_case_charts(op::OperationHistory, ucVM::VariabilityMitigation) = [
-]
+use_case_charts(op::OperationHistory, ucVM::VariabilityMitigation) = []
