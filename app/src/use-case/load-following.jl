2bcb18ed0d30319445efc9037b70eb2baeb9c827
Dexin Wang <dexin.wang@pnnl.gov>
2023-09-01 14:42:19 +0000

    Update load following results

--- app/src/use-case/load-following.jl
+++ app/src/use-case/load-following.jl
@@ -65,16 +65,22 @@ LoadFollowing(input::Dict, tStart::DateTime, tEnd::DateTime) = LoadFollowing(
 )
 
 function calculate_metrics(operation::OperationHistory, ucLF::LoadFollowing)
+    tsNetLoad = ucLF.realtimeLoadPower - power(operation)
+    tsError = tsNetLoad - ucLF.forecastLoadPower
     [
         Dict(:sectionTitle => "Load Following"),
-        Dict(:label => "RMSE", :value => 0),
-        Dict(:label => "Maximum Deviation", :value => 0),
+        Dict(:label => "RMSE", :value => "$(round(sqrt(mean(tsError^2)), sigdigits=2)) kW"),
+        Dict(
+            :label => "Maximum Deviation",
+            :value => "$(round(maximum(abs.(get_values(tsError))), sigdigits=2)) kW",
+        ),
     ]
 end
 
 function use_case_charts(operation::OperationHistory, ucLF::LoadFollowing)
+    tsNetLoad = ucLF.realtimeLoadPower - power(operation)
+    tsRelError = (tsNetLoad - ucLF.forecastLoadPower) / ucLF.forecastLoadPower
     [
-        # TODO: this is an example to be replaced
         Dict(
             :title => "Load Following Performance",
             :height => "400px",
@@ -86,17 +92,23 @@ function use_case_charts(operation::OperationHistory, ucLF::LoadFollowing)
                     :x => timestamps(ucLF.forecastLoadPower),
                     :y => get_values(ucLF.forecastLoadPower),
                     :type => "interval",
-                    :name => "Forecast Load Power",
+                    :name => "Forecast Load",
                 ),
                 Dict(
                     :x => timestamps(ucLF.realtimeLoadPower),
                     :y => get_values(ucLF.realtimeLoadPower),
                     :type => "interval",
-                    :name => "Real-time Load Power",
+                    :name => "Real-time Load",
                 ),
                 Dict(
-                    :x => timestamps(ucLF.realtimeLoadPower),
-                    :y => zeros(length(get_values(ucLF.realtimeLoadPower))), # TODO: to be replaced with actual errors
+                    :x => timestamps(tsNetLoad),
+                    :y => get_values(tsNetLoad),
+                    :type => "interval",
+                    :name => "Real-time Net Load",
+                ),
+                Dict(
+                    :x => timestamps(tsRelError),
+                    :y => get_values(tsRelError),
                     :type => "interval",
                     :name => "Relative Error",
                     :yAxis => "right",
