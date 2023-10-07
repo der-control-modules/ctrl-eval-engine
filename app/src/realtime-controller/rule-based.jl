fe4023c8a13370ed191308798d522d1ea2692e6f
Dexin Wang <dexin.wang@pnnl.gov>
2023-10-07 05:59:19 +0000

    Add support for GF in rule-based RT controller

--- app/src/realtime-controller/rule-based.jl
+++ app/src/realtime-controller/rule-based.jl
@@ -14,9 +14,10 @@ function control(
 )
     scheduledPower = average_power(schedulePeriod)
 
-    idxloadFollowing = findfirst(uc -> uc isa LoadFollowing, useCases)
-    if idxloadFollowing !== nothing
-        ucLF::LoadFollowing = useCases[idxloadFollowing]
+    idxLF = findfirst(uc -> uc isa LoadFollowing, useCases)
+    idxGF = findfirst(uc -> uc isa GenerationFollowing, useCases)
+    if idxLF !== nothing
+        ucLF::LoadFollowing = useCases[idxLF]
         forecastLoad, _, tEndForecstLoad =
             CtrlEvalEngine.get_period(ucLF.forecastLoadPower, t)
         actualLoad, _, tEndActualLoad = CtrlEvalEngine.get_period(ucLF.realtimeLoadPower, t)
@@ -31,6 +32,27 @@ function control(
             min(p_max(ess, tCtrlPeriodEnd - t), actualLoad - theta_high)
         elseif actualLoad < theta_low
             max(p_min(ess, tCtrlPeriodEnd - t), actualLoad - theta_low)
+        else
+            0
+        end
+
+        @debug "Rule-based RT controller exiting" batt_power
+        return FixedIntervalTimeSeries(t, tCtrlPeriodEnd - t, [batt_power])
+    elseif idxGF !== nothing
+        ucGF::GenerationFollowing = useCases[idxGF]
+        forecastPower, _, tEndForecst = CtrlEvalEngine.get_period(ucGF.forecastPower, t)
+        actualPower, _, tEndActual = CtrlEvalEngine.get_period(ucGF.realtimePower, t)
+        forecastNetGen = forecastPower + scheduledPower
+        tCtrlPeriodEnd =
+            isnothing(tEndActual) || isnothing(tEndForecst) ? end_time(schedulePeriod) :
+            min(tEndForecst, tEndActual)
+        theta_low = forecastNetGen - controller.bound
+        theta_high = forecastNetGen + controller.bound
+        η = sqrt(ηRT(ess))
+        batt_power = if actualPower > theta_high
+            min(p_max(ess, tCtrlPeriodEnd - t), theta_high - actualPower)
+        elseif actualPower < theta_low
+            max(p_min(ess, tCtrlPeriodEnd - t), theta_low - actualPower)
         else
             0
         end
