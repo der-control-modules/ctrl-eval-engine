a19fc4beca32ede02313d4d45d0c84d9e6453bf7
Liu, Shane <shane.liu@pnnl.gov>
2024-10-31 10:04:21 -0700

    hydrogen simulator

--- app/src/simulator/hydrogen-energy-storage.jl
+++ app/src/simulator/hydrogen-energy-storage.jl
@@ -1,4 +1,3 @@
-
 struct HydrogenEnergyStorageSystem <: EnergyStorageSystem
     specs::HydrogenEnergyStorageSpecs
     states::HydrogenEnergyStorageStates
@@ -38,14 +37,104 @@ function _operate!(
     hess::HydrogenEnergyStorageSystem,
     powerKw::Real,
     durationHour::Real,
-    ::Real,
+    _::Real,
 )
-    # If `powerkW` is positive, use fuel cell to generate electricity (discharging)
-    #     use H2 from low-pressure tank first, only withdraw from medium-pressure tank if low-pressure is empty
-    # else (`powerkW < 0` charging)
-    #     use electrolyzer to produce hydrogen
-    #     charge low-pressure tank first, only compress and charge medium-pressure tank if low-pressure tank is full
-    #         if compressor is started, reduce electrolyer power to make sure the total power consumption matches `-powerkW`
+    if powerKw > 0
+        h2_needed = powerKw * durationHour / (hess.specs.fuelCellSpecs.efficiencyPu * 39.4)
+        
+        h2_from_lp = min(h2_needed, hess.states.lowPressureH2Kg)
+        hess.states.lowPressureH2Kg -= h2_from_lp
+        
+        if h2_from_lp < h2_needed
+            h2_from_mp = min(
+                h2_needed - h2_from_lp,
+                hess.states.mediumPressureH2Kg
+            )
+            hess.states.mediumPressureH2Kg -= h2_from_mp
+        end
+        
+        hess.states.fuelCellOn = true
+        hess.states.electrolyzerOn = false
+        hess.states.compressorOn = false
+    else
+        h2_produced = -powerKw * durationHour / hess.specs.electrolyzerSpecs.ratePowerKw
+    
+        space_in_lp = hess.specs.hydrogenStorageSpecs.lowPressureTankSizeKg - 
+                     hess.states.lowPressureH2Kg
+        h2_to_lp = min(h2_produced, space_in_lp)
+        hess.states.lowPressureH2Kg += h2_to_lp
+        
+        if h2_to_lp < h2_produced
+            h2_to_mp = min(
+                h2_produced - h2_to_lp,
+                hess.specs.hydrogenStorageSpecs.mediumPressureTankSizeKg - 
+                hess.states.mediumPressureH2Kg
+            )
+            hess.states.mediumPressureH2Kg += h2_to_mp
+            hess.states.compressorOn = true
+        else
+            hess.states.compressorOn = false
+        end
+        
+        hess.states.electrolyzerOn = true
+        hess.states.fuelCellOn = false
+    end
 end
 
-# TODO: implement all exported functions
+function SOC(hess::HydrogenEnergyStorageSystem)
+    total_h2 = hess.states.lowPressureH2Kg + hess.states.mediumPressureH2Kg
+    total_capacity = hess.specs.hydrogenStorageSpecs.lowPressureTankSizeKg +
+                    hess.specs.hydrogenStorageSpecs.mediumPressureTankSizeKg
+    return total_h2 / total_capacity
+end
+
+function energy_state(hess::HydrogenEnergyStorageSystem)
+    total_h2 = hess.states.lowPressureH2Kg + hess.states.mediumPressureH2Kg
+    return total_h2 * 39.4 * hess.specs.fuelCellSpecs.efficiencyPu 
+end
+
+function p_max(hess::HydrogenEnergyStorageSystem, durationHour::Real, _::Real)
+    total_h2 = hess.states.lowPressureH2Kg + hess.states.mediumPressureH2Kg
+    available_energy = total_h2 * 39.4 * hess.specs.fuelCellSpecs.efficiencyPu
+    return min(
+        hess.specs.fuelCellSpecs.ratedPowerKw,
+        available_energy / durationHour
+    )
+end
+
+function p_max(hess::HydrogenEnergyStorageSystem)
+    return hess.specs.fuelCellSpecs.ratedPowerKw
+end
+
+function p_min(hess::HydrogenEnergyStorageSystem, durationHour::Real, _::Real)
+    remaining_capacity = (
+        hess.specs.hydrogenStorageSpecs.lowPressureTankSizeKg - hess.states.lowPressureH2Kg +
+        hess.specs.hydrogenStorageSpecs.mediumPressureTankSizeKg - hess.states.mediumPressureH2Kg
+    )
+    max_h2_intake = remaining_capacity / durationHour
+    return max(
+        -hess.specs.electrolyzerSpecs.ratePowerKw,
+        -max_h2_intake * hess.specs.electrolyzerSpecs.ratePowerKw
+    )
+end
+
+function p_min(hess::HydrogenEnergyStorageSystem)
+    return -hess.specs.electrolyzerSpecs.ratePowerKw
+end
+
+function e_max(hess::HydrogenEnergyStorageSystem)
+    total_capacity = (
+        hess.specs.hydrogenStorageSpecs.lowPressureTankSizeKg +
+        hess.specs.hydrogenStorageSpecs.mediumPressureTankSizeKg
+    )
+    return total_capacity * 39.4 * hess.specs.fuelCellSpecs.efficiencyPu
+end
+
+function e_min(::HydrogenEnergyStorageSystem)
+    return 0.0
+end
+
+function ηRT(hess::HydrogenEnergyStorageSystem)
+    return hess.specs.fuelCellSpecs.efficiencyPu
+end
+
