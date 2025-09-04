05ae47dab317fb900e2b0a3118d9230b06927096
David M. Raker <david.raker@pnnl.gov>
2025-09-04 07:17:20 -0700

    MESA mode for power smoothing. WIP: Needs test.

--- app/src/realtime-controller/mesa-modes/mesa-active-power-smoothing-mode.jl
+++ app/src/realtime-controller/mesa-modes/mesa-active-power-smoothing-mode.jl
@@ -5,8 +5,24 @@ struct ActivePowerSmoothingMode <: MesaMode
     smoothingGradient::Float64
     lowerSmoothingLimit::Float64
     upperSmoothingLimit::Float64
-    smoothingFilterTime::Dates.Second
+    smoothingFilterTime::Dates.Period
     rampParams::RampParams
+end
+
+function ActivePowerSmoothingMode(
+    params::MesaModeParams,
+    smoothingGradient::Float64,
+    lowerSmoothingLimit::Float64,
+    upperSmoothingLimit::Float64,
+    dischargeMaximumRampUp::Float64,
+    dischargeMaximumRampDown::Float64,
+    chargeMaximumRampUp::Float64,
+    chargeMaximumRampDown::Float64
+    )
+return ActivePowerSmoothingMode(
+    params, smoothingGradient, lowerSmoothingLimit, upperSmoothingLimit, smoothingFilterTime,
+    RampParams(Dates.Second(0), Dates.Second(0), dischargeMaximumRampUp, dischargeMaximumRampDown, chargeMaximumRampUp, chargeMaximumRampDown)
+)
 end
 
 
@@ -14,11 +30,26 @@ function modecontrol(
     mode::ActivePowerSmoothingMode,
     ess::EnergyStorageSystem,
     controller::MesaController,
-    schedulePeriod::SchedulePeriod,
+    _,
     useCases::AbstractVector{<:UseCase},
     t::Dates.DateTime,
-    spProgress::VariableIntervalTimeSeries,
-    currentIterationPower::Float64
+    _,
+    _
 )
-    # TODO: Stuff
+    idxPowerSmoothing = findfirst(uc -> uc isa VariabilityMitigation, useCases)
+    if idxPowerSmoothing !== nothing
+        error("Disallowed set of UseCases: Only Variability Mitigation UseCase may be used with the MESA Power Smoothing Mode.")
+    else
+        useCase = useCases[idxPowerSmoothing]
+    end
+    (referencePower, _, _) = get_period(useCase.pvGenProfile, t)
+    movingAveragePower = mean(useCase.pvGenProfile, t - mode.smoothingFilterTime, t)
+    deltaWattage = referencePower - movingAveragePower
+    additionalWatts = lowerSmoothingLimit < deltaWattage < upperSmoothingLimit ? 0.0 : smoothingGradient * deltaWattage
+
+    previousPower = previous_WIP(mode)
+    rampLimitedPower = apply_ramps(ess, mode.rampParams, previousPower, additionalWatts)
+    essLimitedPower = min(max(rampLimitedPower, p_min(ess)), p_max(ess))
+    energyLimitedPower = apply_energy_limits(ess, essLimitedPower, Dates.Second(controller.resolution))
+    return energyLimitedPower
 end
